310fe18f34efae27c24b6b8d997db001e81b1433
Reveriel <reveriel@hotmail.com>
2018-11-28 07:19:59 +0000

    userland makefile

--- userland/params.mk
+++ userland/params.mk
@@ -1,1 +1,10 @@
 SUBDIRS := interactive arch/$(ARCH)/
+
+
+OUTS += test_one.out ksmtrace.out
+
+$(OUT_DIR)/ksmtrace.out: ksm-test/ksmtrace.c
+	$(CC) $(CFLAGS) -o $@ $< -I./ksm-test
+
+$(OUT_DIR)/test_one.out: ksm-test/test_one.c ksm-test/bk.c
+	$(CC) $(CFLAGS) -o $@ ksm-test/test_one.c ksm-test/bk.c  -I./ksm-test
